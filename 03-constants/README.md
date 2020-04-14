93d8607b621ebf6401599a4ca2ac8ac9fcacad6b
Marc Singer <ms@plantrees.ch>
2020-04-14 10:01:04 +0200

    Finished 03-constants

--- 03-constants/README.md
+++ 03-constants/README.md
@@ -6,4 +6,12 @@ Understanding constants, how they can be used and additional compiler rules
 
 ## Results
 
-Work in progress
+* Constants can not change, but can be overwritten in other blocks
+* Values of constants have to be clear at compilation time 
+* They should be named like other variables, because they will be exported as well
+* Typed constants can not change type, except when overwritten in other blocks, untyped could interact with similar types
+* Const blocks work like Enums in other languages
+* IOTA can control the id of enum members
+* IOTA can be set dynamic with Bitshift operations
+
+* It was a lot of fun creating and playing around with constants and enums
--- 03-constants/main.go
+++ 03-constants/main.go
@@ -40,4 +40,46 @@ func main() {
 	fmt.Println(gender == male)
 	fmt.Println(invalidGender == female || invalidGender == male)
 
+	// Constants shift index by 5
+	const (
+		_   = iota + 5
+		foo // 6
+		bar // 7
+	)
+
+	// Example of shifting in constants
+	// This works as long as the value is clearly defined on build time
+	const (
+		_  = iota             // Use default IOTA index to ignore fist item
+		KB = 1 << (10 * iota) // Bit shift 1 with 10 * iota
+		MB
+		GB
+		TB
+		PB
+		EB
+		ZB
+		YB
+	)
+
+	fileSize := 4000000000.
+	fmt.Printf("%.2fGB\n", fileSize/GB)
+
+	// Example to use it as a bitmask for roles
+	// Encoded all roles as one single byte instead
+	const (
+		isAdmin = 1 << iota
+		isHQ
+		canSeeFinances
+		canSeeEurope
+		canSeeAsia
+		canSeeAmerika
+	)
+	// Set role and output
+	roles := isAdmin | canSeeFinances | canSeeEurope | canSeeAsia
+	fmt.Printf("%b\n", roles)
+
+	// Check role
+	fmt.Printf("Has Admin? %v\n", isAdmin&roles == isAdmin)
+	fmt.Printf("Can see finances? %v\n", canSeeFinances&roles == canSeeFinances)
+
 }
