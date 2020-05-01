8d3413462bf51d45304fcd49441f817ca710b71a
Marc Singer <ms@plantrees.ch>
2020-05-01 09:35:22 +0200

    Finished lesson 7

--- 07-type/main.go
+++ 07-type/main.go
@@ -1,15 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
-type canton struct { // Can hold different types of data, anything we want
-	name         string
+// Stucts are copied in memory instead of referenced
+type district struct { // Can hold different types of data, anything we want
+	name         string `max:"100"` // Setting a tag with space delim, key with :, value in quotation marks
 	population   int
 	coronaCases  int
 	coronaDeaths int
 } // A struct can embed other structs
+type country struct {
+	district // Values are the same as in district
+}
 
 func main() {
-	Bern := canton{name: "Bern", population: 1039101, coronaCases: 1708, coronaDeaths: 55}
+	Bern := district{name: "Bern", population: 1039101, coronaCases: 1708, coronaDeaths: 55}
+	Switzerland := country{district: district{name: "Switzerland", population: 8643685, coronaCases: 29586, coronaDeaths: 1423}}
 	fmt.Println(Bern)
+	fmt.Println(Switzerland)
+	// Reflection is used to get tags and parse them in the code
+	// GO does not care about tags
+	// This would be used in a validation lib
+	t := reflect.TypeOf(district{})
+	field, _ := t.FieldByName("name")
+	fmt.Println(field.Tag)
 }
